de09eda5060d7161cfa145a26c790d220eb963be
Matvey Kostukovsky <matvelius@gmail.com>
2019-03-21 19:29:53 -0500

    Initial Commit

---
+++ Meal Tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Meal Tracker
+//
+//  Created by Matvey on 3/21/19.
+//  Copyright © 2019 Matvey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
